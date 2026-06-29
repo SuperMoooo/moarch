516175f90c7a854482c32d96acedac3817379dfe
SuperMoooo <andremontoito@gmail.com>
2026-06-29 15:38:19 +0100

    Readme

--- lib/src/runner.dart
+++ lib/src/runner.dart
@@ -34,7 +34,7 @@ class MoarchRunner {
       final argResults = _runner.parse(args);
 
       if (argResults['version'] == true) {
-        _logger.info('moarch v1.5.8');
+        _logger.info('moarch v1.5.9');
         return 0;
       }
 
